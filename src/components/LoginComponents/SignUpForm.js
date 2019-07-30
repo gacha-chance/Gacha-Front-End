cd64f895ae75afde0251b5573a28f111ab9de4e8
doanmade <43007082+doanmade@users.noreply.github.com>
2019-07-30 13:39:06 -0700

    Login with validation

--- src/components/LoginComponents/SignUpForm.js
+++ src/components/LoginComponents/SignUpForm.js
@@ -1,92 +1,62 @@
 import React from "react";
+import useFormValidation from "./auth/useFormValidation";
+import validateAuth from "./auth/validateAuth";
+import "./styles.css";
+
+
+
+const INITIAL_STATE = {
+  email: "",
+  password: ""
+};
+
+function Register() {
+  const {
+    handleSubmit,
+    handleChange,
+    handleBlur,
+    values,
+    errors,
+    isSubmitting
+  } = useFormValidation(INITIAL_STATE, validateAuth);
 
 export default function SignUpForm() {
   return (
     <div>
       <h1>Sign Up Form</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         {/* Make the onSubmit */}
         <div>
-          <label>First Name </label>
-          <input
-            type="text"
-            id="firstName"
-            className="FormField__Input"
-            placeholder="Enter your full name"
-            name="firstName"
-            value={firstName}
-            onChange={handleChange}
-          />
-        </div>
-
-        <div>
-          <label>Last Name </label>
-          <input
-            type="text"
-            id="lastName"
-            className="FormField__Input"
-            placeholder="Enter your full name"
-            name="lastName"
-            value={lastName}
-            onChange={handleChange}
-          />
-        </div>
-
-        <div>
-          <label>Email</label>
-          <input
-            type="text"
-            id="email"
-            className="FormField__Input"
-            placeholder="Enter your full name"
-            name="email"
-            value={email}
-            onChange={handleChange}
-          />
-        </div>
-
-        <div>
-          <label>UserName</label>
-          <input
-            type="text"
-            id="userName"
-            className="FormField__Input"
-            placeholder="Enter your full name"
-            name="userName"
-            value={userName}
-            onChange={handleChange}
-          />
+          <label>Email Address</label>
+            <input
+              onChange={handleChange}
+              onBlur={handleBlur}
+              name="email"
+              value={values.email}
+              className={errors.email && "error-input"}
+              autoComplete="off"
+              placeholder="Your email address"
+            />
+          {errors.email && <p className="error-text">{errors.email}</p>}
         </div>
 
         <div>
           <label>Password</label>
           <input
-            type="text"
-            id="password"
-            className="FormField__Input"
-            placeholder="Enter your full name"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.password}
+            className={errors.password && "error-input"}
             name="password"
-            value={password}
-            onChange={handleChange}
+            type="password"
+            placeholder="Choose a safe password"
           />
-        </div>
-
-        <div>
-          <label>Re-type Password</label>
-          <input
-            type="text"
-            id="passwordCheck"
-            className="FormField__Input"
-            placeholder="Enter your full name"
-            name="passwordCheck"
-            value={passwordCheck}
-            onChange={handleChange}
-          />
+          {errors.password && <p className="error-text">{errors.password}</p>}
         </div>
 
         <div>
           {/* Need to add onSubmit */}
-          <button>Sign Up</button>
+          <button disabled={isSubmitting} type="submit">Sign Up</button>
         </div>
       </form>
     </div>
--- src/components/LoginComponents/index.js
+++ src/components/LoginComponents/index.js
@@ -1,11 +1,25 @@
-const signUpButton = document.getElementById("signUp");
-const signInButton = document.getElementById("signIn");
-const container = document.getElementById("container");
+// const signUpButton = document.getElementById("signUp");
+// const signInButton = document.getElementById("signIn");
+// const container = document.getElementById("container");
 
-signUpButton.addEventListener("click", () => {
-  container.classList.add("right-panel-active");
-});
+// signUpButton.addEventListener("click", () => {
+//   container.classList.add("right-panel-active");
+// });
 
-signInButton.addEventListener("click", () => {
-  container.classList.remove("right-panel-active");
-});
+// signInButton.addEventListener("click", () => {
+//   container.classList.remove("right-panel-active");
+// });
+import React from "react";
+import LoginForm from "./LoginForm";
+import SignUpForm from "./SignUpForm";
+
+function Forms() {
+  return (
+    <div>
+      <LoginForm />
+      <SignUpForm />
+    </div>
+  );
+}
+
+export default Forms;

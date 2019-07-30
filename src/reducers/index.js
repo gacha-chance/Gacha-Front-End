4d1bdac047819b2b997abafe4546355b38d146ce
Vaeliwen <zaman@imbriaco.com>
2019-07-30 16:54:13 -0400

    Created Redux store.

---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+
+
+const initialState = {
+    user: {
+        username: '',
+        password: '',
+        id: 0,
+    },
+    token: '',
+    saved: [],
+    data: {
+        chance: 0.00,
+        rollNum: 0,
+        compounded: 0.00
+    }
+}
+
+export function reducer(state = initialState, action) {
+    switch(action.type){
+        default:
+            return state
+    }
+}
